6c6cc97f972dc98f1ec116a7f15ca4f7ea589819
Artur <artursmolin@gmail.com>
2018-11-30 17:37:59 +0300

    Update route.rb
    2 spaces

--- Lesson3/route.rb
+++ Lesson3/route.rb
@@ -2,15 +2,15 @@ class Route
   attr_reader :route_list
 		
   def initialize(start_station,end_station)
-	@route_list = [start_station, end_station]
+    @route_list = [start_station, end_station]
   end
 
   def add_station(station)
-	@route_list.insert(-2, station)
+    @route_list.insert(-2, station)
   end
 
   def delete_station(station)
-	@route_list.delete(station) if @route_list.first != station && @route_list.last != station
+    @route_list.delete(station) if @route_list.first != station && @route_list.last != station
   end
 
   def show_stations
