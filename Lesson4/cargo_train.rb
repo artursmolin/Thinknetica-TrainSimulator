bd89551a09732582c2467c902295597f811f5a11
Artur <artursmolin@gmail.com>
2019-01-23 17:29:14 +0300

    2 new classes

--- Lesson4/cargo_train.rb
+++ Lesson4/cargo_train.rb
@@ -2,11 +2,4 @@ class CargoTrain < Train
   def initialize(number,carriage)
     super(number, :cargo, carriage)
   end
-
-
-
-
-  def type_validation?(carriage)
-    carriage.class == CargoCarriage
-  end
 end
