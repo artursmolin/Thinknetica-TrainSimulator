45b02dba3122fe77988d367297eae7699dbf7f81
Artur <artursmolin@gmail.com>
2019-01-28 23:23:47 +0300

    fix wagon.rb

--- Lesson4/wagon.rb
+++ Lesson4/wagon.rb
@@ -1,10 +1,9 @@
-require_relative 'instances.rb'
 require_relative 'manufacturer.rb'
 
 class Wagon
   attr_reader :type
   include Manufacturer
-  
+
   def initialize(type)
     @type = type
     @manufacturer = ""
